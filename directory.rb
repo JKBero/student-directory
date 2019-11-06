36bca48b182c7bfc5f0bd5aa7de3e3f1906337cf
Jade Beresford <jade_fletcher@hotmail.co.uk>
2019-11-06 18:42:27 +0000

    Refactored into shorter methods

--- directory.rb
+++ directory.rb
@@ -1,37 +1,75 @@
+@students = []  # an empty array accessible to all methods
+
 def input_students
   puts "Please enter the names of the students and the month of their cohort"
   puts "To finish, just hit return twice"
-  # create an empty array
-  students = []
   # get the first name
   puts "Name of student:"
   name = gets.chomp
   # while the name is not empty, repeat this code
   while !name.empty? do
     # get the cohort
-    puts "Cohort of student:"
-    cohort = gets.chomp.capitalize
-    # if cohort is empty, sets default month
-    if cohort.empty? then cohort = "January" end
-    # if an invalid month/typo entered, it redoes loop
-    require 'date'
-    if !Date::MONTHNAMES.include?(cohort)
-      puts "Invalid month given"
-      redo
-    end
+    cohort = cohort_selection
+    if cohort == false then redo end
     # add the student hash to the array
-    students << {name: name, cohort: cohort.to_sym}
-    if students.count == 1
-      puts "Now we have #{students.count} student"
+    @students << {name: name, cohort: cohort.to_sym}
+    if @students.count == 1
+      puts "Now we have #{@students.count} student"
     else
-      puts "Now we have #{students.count} students"
+      puts "Now we have #{@students.count} students"
     end
     # get another name from the user
     puts "Name of student:"
     name = gets.chomp
   end
-  # return the array of students
-  students
+end
+
+def interactive_menu
+  loop do
+    print_menu
+    process(gets.chomp)
+  end
+end
+
+def print_menu
+  puts "1. Input the students"
+  puts "2. Show the students"
+  puts "9. Exit"
+end
+
+def show_students
+  if !@students.empty?
+    print_header
+    print_student_list
+    print_footer
+  else
+    puts "The student list is empty"
+  end
+end
+
+def process(selection)
+  case selection
+    when "1"
+      input_students
+    when "2"
+      show_students
+    when "9"
+      exit # this will cause the program to terminate
+    else
+      puts "I don't know what you meant, try again"
+  end
+end
+
+def cohort_selection
+  puts "Cohort of student:"
+  cohort = gets.chomp.capitalize
+  # if an invalid month/typo entered, it returns false
+  require 'date'
+  if !Date::MONTHNAMES.include?(cohort)
+    puts "Invalid month given"
+    false
+  else cohort
+  end
 end
 
 def print_header
@@ -39,44 +77,17 @@ def print_header
   puts "-------------"
 end
 
-def print(students)
-  students.each { |student|
+def print_student_list
+  @students.each { |student|
     puts "#{student[:name]} (#{student[:cohort]} cohort)"
   }
 end
 
-def print_footer(students)
-  if students.count == 1
-    puts "Overall, we have #{students.count} great student"
+def print_footer
+  if @students.count == 1
+    puts "Overall, we have #{@students.count} great student"
   else
-    puts "Overall, we have #{students.count} great students"
-  end
-end
-
-def interactive_menu
-  students = []
-  loop do
-    #1. print the menu and ask the user what to do
-    puts "1. Input the students"
-    puts "2. Show the students"
-    puts "9. Exit"
-    #2. read the input and save it into a variable
-    selection = gets.chomp
-    #3. do what the user has asked
-    case selection
-      when "1"
-        students = input_students
-      when "2"
-        if !students.empty?
-          print_header
-          print(students)
-          print_footer(students)
-        end
-      when "9"
-        exit # this will cause the program to terminate
-      else
-        puts "I don't know what you meant, try again"
-    end
+    puts "Overall, we have #{@students.count} great students"
   end
 end
 
