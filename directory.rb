13b3feca748518c4e0d38d456c5e0e8543e2ba0e
Jade Beresford <jade_fletcher@hotmail.co.uk>
2019-11-06 17:57:24 +0000

    User can choose cohort, and empty list will not be printed

--- directory.rb
+++ directory.rb
@@ -1,16 +1,33 @@
 def input_students
-  puts "Please enter the names of the students"
+  puts "Please enter the names of the students and the month of their cohort"
   puts "To finish, just hit return twice"
   # create an empty array
   students = []
   # get the first name
+  puts "Name of student:"
   name = gets.chomp
   # while the name is not empty, repeat this code
   while !name.empty? do
+    # get the cohort
+    puts "Cohort of student:"
+    cohort = gets.chomp.capitalize
+    # if cohort is empty, sets default month
+    if cohort.empty? then cohort = "January" end
+    # if an invalid month/typo entered, it redoes loop
+    require 'date'
+    if !Date::MONTHNAMES.include?(cohort)
+      puts "Invalid month given"
+      redo
+    end
     # add the student hash to the array
-    students << {name: name, cohort: :november}
-    puts "Now we have #{students.count} students"
+    students << {name: name, cohort: cohort.to_sym}
+    if students.count == 1
+      puts "Now we have #{students.count} student"
+    else
+      puts "Now we have #{students.count} students"
+    end
     # get another name from the user
+    puts "Name of student:"
     name = gets.chomp
   end
   # return the array of students
@@ -29,11 +46,17 @@ def print(students)
 end
 
 def print_footer(students)
-  puts "Overall, we have #{students.count} great students"
+  if students.count == 1
+    puts "Overall, we have #{students.count} great student"
+  else
+    puts "Overall, we have #{students.count} great students"
+  end
 end
 
 students = input_students
 # nothing happens until we call the methods
-print_header
-print(students)
-print_footer(students)
+if !students.empty?
+  print_header
+  print(students)
+  print_footer(students)
+end
