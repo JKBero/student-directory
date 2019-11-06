f25d169443fd275a9a2e7f0d0ae0befaa9227df2
Jade Beresford <jade_fletcher@hotmail.co.uk>
2019-11-06 21:40:24 +0000

    Saving student data to text.csv

--- directory.rb
+++ directory.rb
@@ -3,7 +3,6 @@
 def input_students
   puts "Please enter the names of the students and the month of their cohort"
   puts "To finish, just hit return twice"
-  # get the first name
   puts "Name of student:"
   name = gets.chomp
   # while the name is not empty, repeat this code
@@ -11,14 +10,12 @@ def input_students
     # get the cohort
     cohort = cohort_selection
     if cohort == false then redo end
-    # add the student hash to the array
     @students << {name: name, cohort: cohort.to_sym}
     if @students.count == 1
       puts "Now we have #{@students.count} student"
     else
       puts "Now we have #{@students.count} students"
     end
-    # get another name from the user
     puts "Name of student:"
     name = gets.chomp
   end
@@ -34,6 +31,7 @@ end
 def print_menu
   puts "1. Input the students"
   puts "2. Show the students"
+  puts "3. Save the list to students.csv"
   puts "9. Exit"
 end
 
@@ -53,6 +51,8 @@ def process(selection)
       input_students
     when "2"
       show_students
+    when "3"
+      save_students
     when "9"
       exit # this will cause the program to terminate
     else
@@ -89,6 +89,18 @@ def print_footer
   else
     puts "Overall, we have #{@students.count} great students"
   end
+end
+
+def save_students
+  # open the file for writing
+  file = File.open("students.csv", "w")
+  # iterate over the array of students
+  @students.each { |student|
+    student_data = [student[:name], student[:cohort]]
+    csv_line = student_data.join(",")
+    file.puts csv_line
+  }
+  file.close
 end
 
 interactive_menu
