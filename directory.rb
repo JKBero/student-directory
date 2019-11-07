3bc4bfb617d575903d4f787ffc13038462bf6ca3
Jade Beresford <jade_fletcher@hotmail.co.uk>
2019-11-07 13:28:36 +0000

    Code refactored

--- directory.rb
+++ directory.rb
@@ -5,13 +5,13 @@ def print_menu
   puts "2. Show the students"
   puts "3. Save the list to students.csv"
   puts "4. Load the list from students.csv"
-  puts "9. Exit"
+  puts "5. Exit"
 end
 
 def interactive_menu
   loop do
     print_menu
-    process(gets.chomp)
+    process(STDIN.gets.chomp)
   end
 end
 
@@ -25,7 +25,7 @@ def process(selection)
       save_students
     when "4"
       load_students
-    when "9"
+    when "5"
       exit # this will cause the program to terminate
     else
       puts "I don't know what you meant, try again"
@@ -42,12 +42,8 @@ def input_students
     # get the cohort
     cohort = cohort_selection
     if cohort == false then redo end
-    @students << {name: name, cohort: cohort.to_sym}
-    if @students.count == 1
-      puts "Now we have #{@students.count} student"
-    else
-      puts "Now we have #{@students.count} students"
-    end
+    update_student_list(name, cohort)
+    print_student_count
     # get another name from the user
     puts "Name of student:"
     name = STDIN.gets.chomp
@@ -66,11 +62,15 @@ def cohort_selection
   end
 end
 
+def update_student_list(name, cohort)
+  @students << {name: name, cohort: cohort.to_sym}
+end
+
 def show_students
   if !@students.empty?
     print_header
     print_student_list
-    print_footer
+    print_student_count
   else
     puts "The student list is empty"
   end
@@ -87,11 +87,11 @@ def print_student_list
   }
 end
 
-def print_footer
+def print_student_count
   if @students.count == 1
-    puts "Overall, we have #{@students.count} great student"
+    puts "Overall, we have #{@students.count} student"
   else
-    puts "Overall, we have #{@students.count} great students"
+    puts "Overall, we have #{@students.count} students"
   end
 end
 
@@ -105,36 +105,30 @@ def save_students
     file.puts csv_line
   }
   file.close
+  puts "Data successfully saved"
 end
 
-<<<<<<< HEAD
 def load_students(filename = "students.csv")
   file = File.open(filename, "r")
-=======
-def load_students
-  file = File.open("students.csv", "r")
->>>>>>> fde56507e2e12aa04a236884469d7a683a5a57db
   file.readlines.each { |line|
     name, cohort = line.chomp.split(",")
-    @students << {name: name, cohort: cohort.to_sym}
+    update_student_list(name, cohort)
   }
   file.close
+  puts "Data successfully loaded"
 end
 
-<<<<<<< HEAD
-def try_load_students
+def load_students_on_startup # if a file is provided by user
   filename = ARGV.first # first argument from the command line
   return if filename.nil? # get out of the method if it isn't given
   if File.exists?(filename) # if it exists
     load_students(filename)
-    puts "Loaded #{@students.count} from #{filename}"
+    puts "Loaded #{@students.count} students from #{filename}"
   else # if it doesn't exist
     puts "Sorry, #{filename} doesn't exist."
     exit # quit the program
   end
 end
 
-try_load_students
-=======
->>>>>>> fde56507e2e12aa04a236884469d7a683a5a57db
+load_students_on_startup
 interactive_menu
